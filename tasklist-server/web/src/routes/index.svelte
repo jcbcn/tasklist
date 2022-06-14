c6d2c3f79b1778058602dfd5023b3df7e54ae9bb
jcbcn <jcbcn@users.noreply.github.com>
2022-06-14 03:46:43 +0100

    style: More minor adjustments

--- tasklist-server/web/src/routes/index.svelte
+++ tasklist-server/web/src/routes/index.svelte
@@ -16,8 +16,8 @@
 					<div class="task-list-item w-full">
 						Starbucks Coffee
 						<span
-							class="float-right inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-							>5 LP</span
+							class="float-right inline-block bg-red-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+							>-5 LP</span
 						>
 					</div>
 				</div>
@@ -27,8 +27,8 @@
 					<div class="task-list-item w-full">
 						Takeaway Pizza
 						<span
-							class="float-right inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-							>10 LP</span
+							class="float-right inline-block bg-red-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+							>-10 LP</span
 						>
 					</div>
 				</div>
@@ -49,11 +49,31 @@
 						<div class="radio-button-container mr-2">
 							<div class="radio-button" />
 						</div>
-						Test
+						Couch to 5k
 						<span
 							class="float-right inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-							>0 LP</span
+							>2 LP</span
 						>
+
+						<div class="text-red-500 text-xs">
+							<span
+								><svg
+									xmlns="http://www.w3.org/2000/svg"
+									class="h-3 w-3 inline"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+									stroke-width="2"
+								>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+									/>
+								</svg>
+							</span>
+							<span>Yesterday</span>
+						</div>
 					</div>
 					<div class="task-list-item w-full border-b-2 border-gray-200 px-2 py-3 hover:bg-gray-50">
 						<div class="radio-button-container mr-2">
@@ -101,6 +121,11 @@
 		<div class="mt-5">
 			<div class="flex items-center justify-between">
 				<h1 class="text-2xl text-gray-900 font-bold">Today</h1>
+				<a href="#" class="text-sm font-medium text-indigo-600 hover:text-indigo-500 underline"
+					><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+					  </svg> Export to app</a
+				>
 			</div>
 			<div class="py-6 mb-6">
 				<div class="rounded-lg">
@@ -294,7 +319,6 @@
 							>0 LP</span
 						>
 					</div>
-
 				</div>
 			</div>
 		</div>
